483783603c285a78e4856aeb785845445cb16146
bienvenu7 <ngadzieb@gmail.com>
2023-04-16 21:36:26 +0300

    before merge

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,58 +1,71 @@
-import { useNavigation } from '@react-navigation/native'
-import React from 'react'
-import { FlatList, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import { Icon } from 'react-native-elements'
-import { useSelector } from 'react-redux'
-import tw from 'tailwind-react-native-classnames'
-import { selectOrigin } from '../slices/navSlices'
+import { useNavigation } from "@react-navigation/native";
+import React from "react";
+import {
+  FlatList,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { Icon } from "react-native-elements";
+import { useSelector } from "react-redux";
+import tw from "tailwind-react-native-classnames";
+import { selectOrigin } from "../slices/navSlices";
 
 const data = [
-    {
-        id: "100",
-        title: "Get a ride",
-        image: "https://links.papareact.com/3pn",
-        screen: "MapScreen"
-    },
-    {
-        id: "200",
-        title: "Order food",
-        image: "https://links.papareact.com/28w",
-        screen: "EatsScreen"
-    }
-]
+  {
+    id: "100",
+    title: "Get a ride",
+    image: "https://links.papareact.com/3pn",
+    screen: "MapScreen",
+  },
+  {
+    id: "200",
+    title: "Order food",
+    image: "https://links.papareact.com/28w",
+    screen: "EatsScreen",
+  },
+];
 
 const NavOptions = () => {
-
-    const navigation = useNavigation();
-    const origin = useSelector(selectOrigin)
+  const navigation = useNavigation();
+  const origin = useSelector(selectOrigin);
 
   return (
     <FlatList
-        data={data}
-        horizontal
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => (
-            <TouchableOpacity disabled={!origin} onPress={() => navigation.navigate(item.screen)} style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
-                <View>
-                    <Image
-                        style={{
-                            width: 120,
-                            height: 120,
-                            resizeMode: "contain",
-                        }}
-                        source={{ uri: item.image }}
-                    />
-                    <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
-                    <Icon 
-                        style={tw`p-2 bg-black rounded-full w-10 mt-4`}
-                        name='arrowright' color="white" type='antdesign'/>
-                </View>
-            </TouchableOpacity>
-        )}
+      data={data}
+      horizontal
+      keyExtractor={(item) => item.id}
+      renderItem={({ item }) => (
+        <TouchableOpacity
+          disabled={!origin}
+          onPress={() => navigation.navigate(item.screen)}
+          style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
+        >
+          <View>
+            <Image
+              style={{
+                width: 120,
+                height: 120,
+                resizeMode: "contain",
+              }}
+              source={{ uri: item.image }}
+            />
+            <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
+            <Icon
+              style={tw`p-2 bg-black rounded-full w-10 mt-4`}
+              name="arrowright"
+              color="white"
+              type="antdesign"
+            />
+          </View>
+        </TouchableOpacity>
+      )}
     />
-  )
-}
+  );
+};
 
-export default NavOptions
+export default NavOptions;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
